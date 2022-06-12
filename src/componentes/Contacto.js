89d910960ad6372b4777d5c3740d2775ad29b497
Gabriel Agustin Ledezma <gabrielledezma@outlook.com>
2022-06-12 00:23:11 -0300

    cambio en redireccionamiento

--- src/componentes/Contacto.js
+++ src/componentes/Contacto.js
@@ -6,9 +6,9 @@ export default function Contacto() {
     <>
       <section className="contenedor contacto">
         <h2>Contacto</h2>
-        <p><img className="logoFooter" src={logoMail} alt="logo" /> <a className="link" href="https://www.outlook.com">gabrielledezma@outlook.com</a></p>
-        <p><img className="logoFooter" src={logolinkedin} alt="logo" /> <a className="link" href="https://www.linkedin.com/in/gabrielagustinledezma/">Gabriel Agustin Ledezma</a></p>
-        <p><img className="logoFooter" src={logoGit} alt="logo" /> <a className="link" href="https://github.com/gabiledezma">gabiledezma</a></p>
+        <p><img className="logoFooter" src={logoMail} alt="logo" /> <a className="link" href="https://www.outlook.com" target="_blank">gabrielledezma@outlook.com</a></p>
+        <p><img className="logoFooter" src={logolinkedin} alt="logo" /> <a className="link" href="https://www.linkedin.com/in/gabrielagustinledezma/" target="_blank">Gabriel Agustin Ledezma</a></p>
+        <p><img className="logoFooter" src={logoGit} alt="logo" /> <a className="link" href="https://github.com/gabiledezma" target="_blank">gabiledezma</a></p>
       </section>
     </>
   );
